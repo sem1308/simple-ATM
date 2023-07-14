d4ba994a85ded96efcb599773a9048c2029b3a4f
sem1308 <hshhan1009@naver.com>
2023-07-14 20:16:30 +0900

    update: update annotation

--- main.py
+++ main.py
@@ -17,9 +17,6 @@ if __name__ == '__main__':
                           "account_num" : "444412341234",
                         "balance" : 4379200}
     }
-    # 4579-7300-7124-7055
-    # 24/08
-    # 302
     dummy_cards = {
         "4579730071247055" : {
             "valid_thru": ["24", "08"],
@@ -29,6 +26,11 @@ if __name__ == '__main__':
                           dummy_accounts["333312341234"]]
         }
     }
+    # input below for card information
+    # card number : 4579-7300-7124-7055
+    # valid thru : 24/08
+    # cvc : 302
+    # pin number : 1234
     cash = 10**6 # assume that 1,000,000$ is in ATM / 100만 $가 ATM기기에 있다 가정
     machine = ATM(cash,BANK(dummy_accounts),CARD(dummy_cards))
     machine.run()
